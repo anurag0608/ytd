3cd11e7af4ec3bcebcc5d34ebf846efce07625b3
Anurag Verma <anuragv.1020@gmail.com>
2020-04-06 05:02:10 +0530

    improved the UI of redirect page

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -1,3 +1,28 @@
+const givesomefacts = ()=>{
+    const $ = jQuery.noConflict();
+    const facts = $('#random_facts')
+
+    setTimeout(()=>{
+        $(facts).addClass('text-blur-out');
+    },2000)
+    setTimeout(()=>{
+        $(facts).removeClass('text-blur-out');
+        $(facts).addClass('slide-in-top');
+        $(facts).text("Here are some facts for you :)")
+    },3000)
+    interval = setInterval(()=>{
+        console.log('running')
+        let random = Math.ceil(Math.random()*(100000)+0)
+        $(facts).removeClass('text-focus-in');
+        $.get(`http://numbersapi.com/random/year`, function(data) {
+            $(facts).removeClass('slide-in-top');
+            $(facts).addClass('text-focus-in');
+            setTimeout(()=>{
+            $(facts).text(data);
+            },0.5)
+        });
+    },5000)
+}
 $(document).ready(()=>{
     const bar = $('#js-progressbar')
     const btn = $('#autofire')
@@ -6,14 +31,14 @@ $(document).ready(()=>{
         url: $(btn).attr('url'),
         title: $(btn).text(),
     }
+    let interval = null
     console.log(data)
     const socket = io.connect(`${window.location.host}`)
     let statusFlag = false
-    
-    socket.emit('downloadInfo',data);
 
-   
-    
+    if(data.url&&data.quality&&data.title)
+    socket.emit('downloadInfo',data);
+    givesomefacts()
     socket.on('progress',progress=>{
         console.log(progress)
         if(!statusFlag){ console.log("Downloading audio now..."); statusFlag=true}
@@ -26,6 +51,7 @@ $(document).ready(()=>{
     })
     socket.on('downloadlink',(link)=>{
         window.location.href = link
+        clearInterval(interval)
     })
-   
+  
 })
